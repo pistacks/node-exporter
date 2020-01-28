a75e51ed84edd66a5b4c3897f3d564ef3c521480
pistacks <54232559+pistacks@users.noreply.github.com>
2020-01-28 19:57:59 +0200

    Create docker-entrypoint.sh

---
+++ conf/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+# credit:
+# https://github.com/stefanprodan/swarmprom/tree/master/node-exporter
+
+NODE_NAME=$(cat /etc/nodename)
+echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
+
+set -- /bin/node_exporter "$@"
+
+exec "$@"
